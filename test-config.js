ccf644bcfcef390a900a7a41175e6774d4b2e0c5
mathchamps-akademik <nurul.ikhsani@ruangguru.com>
2026-06-17 08:24:08 +0700

    Update test-config.js

--- test-config.js
+++ test-config.js
@@ -1,6 +1,6 @@
 window.MATHCHAMPS_TEST_CONFIG = {
   // Paste URL Web App Google Apps Script di bawah ini agar hasil submit masuk ke spreadsheet.
-  scriptUrl: "",
+  scriptUrl: "https://script.google.com/macros/s/AKfycbyTjO_4uyhwm2ANGHuOzCrNpeGIPUzrp596Z1OBvlR6mfdns5m7OHRpqkWDQ-YFQrkD/exec",
 
   // Isi nama guru secara manual di daftar ini. Nama akan muncul sebagai dropdown di halaman test.
   teachers: [
